7656d37383f2142288100400334f80431add50dd
Sezgin Cholak <sezgin.cholak@gmail.com>
2015-03-14 19:00:35 +0200

    my commit message

--- module/Brzilia/src/Brzilia/Controller/IndexController.php
+++ module/Brzilia/src/Brzilia/Controller/IndexController.php
@@ -10,4 +10,9 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+
+    public function testAction()
+    {
+        return new ViewModel();
+    }
 }
